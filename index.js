87781c0511b4748a62576ecaad5532a41b4bd74d
TuttiFrutti1090 <tuttifruttigaming321@gmail.com>
2021-03-04 09:14:04 +0000

    test

---
+++ index.js
@@ -0,0 +1,8 @@
+function openPanel()
+{
+    document.getElementById("hidden_panel").style.display = "block";
+}
+function closePanel() 
+{
+    document.getElementById("hidden_panel").style.display = "none";
+}
